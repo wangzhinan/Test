2339e6448dee3f3313ccc2dc0a672473d0a100bb
wangzn <wangzn@sunlandgroup.cn>
2018-10-28 10:25:05 +0800

    revert

--- app/src/main/java/com/sunland/test/MainActivity.java
+++ app/src/main/java/com/sunland/test/MainActivity.java
@@ -9,7 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*<<<<<<< HEAD
         // modify by A at 9:24 2018/10/28
         //modify by A at 9:35 2018/10/28
+=======
+>>>>>>> parent of 80e5f85... first modify by A*/
     }
 }
