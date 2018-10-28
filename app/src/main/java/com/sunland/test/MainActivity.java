80e5f8550a8ee336105ae87b1e21d2f8a7eadb6b
wangzn <wangzn@sunlandgroup.cn>
2018-10-28 09:25:28 +0800

    first modify by A

--- app/src/main/java/com/sunland/test/MainActivity.java
+++ app/src/main/java/com/sunland/test/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // modify by A at 9:24 2018/10/28
     }
 }
